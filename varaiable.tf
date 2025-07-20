b31a37e2eacfe6295b15c87413aeffa7c740472f
sriramkausik <45325612+sriramkausik@users.noreply.github.com>
2025-07-20 13:57:00 +0000

    Update varaiable.tf

--- varaiable.tf
+++ varaiable.tf
@@ -14,19 +14,19 @@ variable "SecretAccessKey"{
     default = "Ld2nrybalySDYXVn+uOeo5ag/zBc2qrAm6GUK00I"
 }
 
-output Ansible_Master_Server{
-    value = [aws_instance.Ansible-Master.public_ip,aws_instance.Ansible-Master.private_ip]
-}
+# output Ansible_Master_Server{
+#     value = [aws_instance.Ansible-Master.public_ip,aws_instance.Ansible-Master.private_ip]
+# }
 
-output Windows_server-1 {
-    value = aws_instance.EC2FROMTF.public_ip
-}
+# output Windows_server-1 {
+#     value = aws_instance.EC2FROMTF.public_ip
+# }
 
-output Windows_server-2 {
-    value = aws_instance.Win-2.public_ip
-}
+# output Windows_server-2 {
+#     value = aws_instance.Win-2.public_ip
+# }
 
 
-output Windows_server-3{
-    value = aws_instance.Win-3.public_ip
-}
+# output Windows_server-3{
+#     value = aws_instance.Win-3.public_ip
+# }
