af4248f80da5ab979382e15b2005d4e6f28cc8fb
sriramkausik <45325612+sriramkausik@users.noreply.github.com>
2025-07-20 12:56:54 +0000

    Update varaiable.tf

--- varaiable.tf
+++ varaiable.tf
@@ -7,11 +7,11 @@ variable "Subnet"{
 }
 
 variable "AccessKeyID"{
-    default = "AKIATC6AQYSJXK5F3UH5"
+    default = "AKIA47CRWHENBXNM2VUE"
 }
 
 variable "SecretAccessKey"{
-    default = "0VJmB0+bTKQpl0mAWpsYL4QoP2qIGin8Lwx2BtHi"
+    default = "7DQCsfCtWhynZT/oU+uJeh2Y624viP6JKhPJtnvM"
 }
 
 output Ansible_Master_Server{
